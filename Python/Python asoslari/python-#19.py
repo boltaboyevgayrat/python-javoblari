00d1f37164803c60111a0e9b33f2f05a82f55b6f
boltaboyevgayrat <boltaboyevgayrat305@gmail.com>
2026-03-11 19:54:18 +0500

    yangilandi

---
+++ Python/Python asoslari/python-#19.py
@@ -0,0 +1,42 @@
+# def salom_ber():
+#     """Salom beruvchi funksiya"""
+#     print('Assalomu alaykum!')
+
+# salom_ber()
+
+# def salom_ber(ism):
+#     """Foydalanuvchi ismini qabul qilib, unga salom beruvchi funksiya"""
+#     print(f'Assalomu alaykum, hurmatli {ism.title()}!')
+
+# salom_ber('hasan')
+
+# salom_ber()
+
+# def salom_ber(ism):
+#     """Foydalanuvchi ismini qabul qilib, 
+#     unga salom beruvchi funksiya"""
+#     print(f'Assalomu alaykum, hurmatli {ism.title()}!')
+
+# print(salom_ber.__doc__)
+
+# salom_ber('hasan')
+# salom_ber('olim')
+
+# def toliq_ism(ism, familiya):
+#     """Foydalanuvchi ism familiyasini jamlab chiqaruvchi funksiya"""
+#     print(f'Foydalanuvchi ismi: {ism.title()}')
+#     print(f'Foydalanuvchi familiyasi: {familiya.title()}')
+
+# toliq_ism('olim', 'hakimov')
+
+# def yosh_hisobla(ism, t_yil):
+#     """Foydalanuvchi yoshini hisoblaydigan dastur"""
+#     print(f'{ism.title()} {2026 - t_yil} yoshda.')
+
+# yosh_hisobla('olim', 2005)
+
+# yosh_hisobla(2002, 'olim')
+
+# yosh_hisobla(t_yil=2002, ism='olim')
+
+# toliq_ism(familiya='hakimov', ism='olim')
