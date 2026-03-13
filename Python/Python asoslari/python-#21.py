f05c6dc475e7219dc8c3bb4390858abe5fff1835
boltaboyevgayrat <boltaboyevgayrat305@gmail.com>
2026-03-13 05:56:41 +0500

    yangilandi

---
+++ Python/Python asoslari/python-#21.py
@@ -0,0 +1,47 @@
+# def bahola(ismlar):
+#     baholar ={}
+#     while ismlar:
+#         ism = ismlar.pop()
+#         baho = input(f'Talaba {ism.title()}ning bahosini kiriting: ')
+#         baholar[ism] = int(baho)
+#     return baholar
+
+# talabalar = ['ali', 'vali', 'hasan', 'husan']
+# baholar = bahola(talabalar[:])
+
+# print(baholar)
+
+# print(talabalar)
+
+# def katta_harf(royhat):
+#     for i in range(len(royhat)):
+#         royhat[i] = royhat[i].title()
+
+# ismlar = ['ali', 'vali', 'hasan', 'husan']
+# katta_harf(ismlar)
+# print(ismlar)
+
+# def katta_harf(royhat):
+#     yangi_royhat = []
+#     for ism in royhat:
+#         yangi_royhat.append(ism.title())
+
+#     return yangi_royhat
+# ismlar = ['ali', 'vali', 'hasan', 'husan']
+# yangi_ismlar = katta_harf(ismlar)
+# print(ismlar)
+# print(yangi_ismlar)
+
+# def bahola(ismlar):
+#     baholar ={}
+#     for ism in ismlar:
+#         baho = input(f'Talaba {ism.title()}ning bahosini kiriting: ')
+#         baholar[ism] = int(baho)
+#     return baholar
+
+# talabalar = ['ali', 'vali', 'hasan', 'husan']
+# baholar = bahola(talabalar[:])
+
+# print(baholar)
+
+# print(talabalar)
